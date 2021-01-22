4d772256c2a5c45214a10274d3515cba9f018a0d
Elbatouri badr-eddine <elbatouri.b@gmail.com>
2021-01-22 10:48:19 +0000

    Reworked code
    Reworked front-end, whit simple login/register/logout bringing info from the categories from the database. its just the old code but More clean code,more organised and ready for team work, some performance boost. we agree on a DIR architecture so we dont get dizzy whit a lot of files.

--- HOMEPAGE/index-php-process.php
+++ HOMEPAGE/index-php-process.php
@@ -1,46 +1,45 @@
-<<<<<<< HEAD:header.php
-<?php
-ob_start();
-session_start();
-
-include("admin/inc/config.php");
 
 
-
-$error_message = '';
-$success_message = '';
-$error_message1 = '';
-$success_message1 = '';
-=======
->>>>>>> tests:HOMEPAGE/index-php-process.php
-
-// Kijib loghat likaynin men DB
-
-$i=1;
 
 
 <?php
 
 
-$statement = $pdo->prepare("SELECT * FROM tbl_top_category");
+
+
+if (!isset($_REQUEST['id']) || !isset($_REQUEST['type']))
+{
+    $isAll = true;
+
+    
+
+} else {
+    $isAll = false;
+}
+
+
+{	
+	$statement = $pdo->prepare("SELECT * FROM tbl_top_category");
 	$statement->execute();
 	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
 	foreach ($result as $row) {
-		$top_cat_id[] = $row['tcat_id'];
-		$top_cat_name[] = $row['tcat_name'];
+		$top[] = $row['tcat_id'];
+		$top1[] = $row['tcat_name'];
 	}
 
 
 	$statement = $pdo->prepare("SELECT * FROM tbl_mid_category");
 	$statement->execute();
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
 	foreach ($result as $row) {
 		$mid_cat_id[] = $row['mcat_id'];
 		$mid_cat_name[] = $row['mcat_name'];
-		$mid_top_cat_id[] = $row['tcat_id'];
+		$mid_to_top_cat_id[] = $row['tcat_id'];
 	}
+
 
 	$statement = $pdo->prepare("SELECT * FROM tbl_end_category");
 	$statement->execute();
@@ -48,18 +47,40 @@ $statement = $pdo->prepare("SELECT * FROM tbl_top_category");
 	foreach ($result as $row) {
 		$end_cat_id[] = $row['ecat_id'];
 		$end_cat_name[] = $row['ecat_name'];
-		$end_mid_cat_id[] = $row['mcat_id'];
+		$end_to_mid_cat_id[] = $row['mcat_id'];
 	}
 
+if(!$isAll){
+	if ($_REQUEST['type'] == 'top-category') {
+    
+            $arr1 = array();
+            $arr2 = array();
+
+			for ($i = 0; $i < count($mid_to_top_cat_id); $i++) {
+				if ($mid_to_top_cat_id[$i] == $_REQUEST['id']) {
+                    $arr1[] = $mid_cat_id[$i];
+                    $arr2[] = $mid_cat_name[$i];
+				}
+            }
+            $mid_cat_id = $arr1;
+            $mid_cat_name = $arr2;
+            foreach($mid_cat_id as $id){
+                foreach( $end_cat_id as $end_id){
+                    if($id == $end_id){
+                        $final_ecat_ids[] = $end_id;
+                    }
+                }
+            }
+		
+    }
+}
 
 
-<<<<<<< HEAD:header.php
+        
+        
+}
+        
+
+
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-</head>
-=======
-?>
->>>>>>> tests:HOMEPAGE/index-php-process.php
